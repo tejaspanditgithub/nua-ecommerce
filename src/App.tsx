cc1f827b3752ad34f429b4818fe5d082e043f585
Tejas Pandit <tejas@Tejass-MacBook-Air.local>
2026-06-11 12:29:28 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@ import './App.css'
 import React from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
   return (
-    <div>App</div>
+    <>
+      <div>
+        <h1>Basic counter</h1>
+        <div>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+      </div>
+    </>
   )
 }
 
