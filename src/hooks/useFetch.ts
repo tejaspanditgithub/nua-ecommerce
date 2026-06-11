98e763c9b295518220603b503993b74f7d348b9e
Tejas Pandit <tejas@Tejass-MacBook-Air.local>
2026-06-11 12:14:55 +0530

    initial empty repo

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import { useState,useEffect } from 'react'
+
+
+
+export const useFetch = (url: string) => {
+  const [data, setData] = useState<any>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true)
+        const response = await axios.get(url)
+        setData(response.data)
+      } catch (err) {
+        setError('Failed to fetch data')
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [url])
+
+  return { data, loading, error }
+}
